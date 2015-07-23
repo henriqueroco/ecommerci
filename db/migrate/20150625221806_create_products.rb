b8faa43308986254cd0a90ff6770dfa761d4747b
henriqueroco <henrique.roco@gmail.com>
2015-07-23 20:42:16 -0300

    alteração migrates

--- db/migrate/20150625221806_create_products.rb
+++ db/migrate/20150625221806_create_products.rb
@@ -1,10 +1,10 @@
 class CreateProducts < ActiveRecord::Migration
   def change
-    create_table :products do |t|
-      t.string :name
-      t.integer :category_id
-      t.decimal :value
-      t.boolean :status
+    create_table  :products do |t|
+      t.string    :name
+      t.integer   :category_id
+      t.decimal   :value
+      t.boolean   :status
 
       t.timestamps null: false
     end
