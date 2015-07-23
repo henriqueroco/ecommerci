72f03c5e785b3c7e98208730f83e04f6149aee25
henriqueroco <henrique.roco@gmail.com>
2015-07-23 20:00:43 -0300

    init

---
+++ db/migrate/20150625221806_create_products.rb
@@ -0,0 +1,12 @@
+class CreateProducts < ActiveRecord::Migration
+  def change
+    create_table :products do |t|
+      t.string :name
+      t.integer :category_id
+      t.decimal :value
+      t.boolean :status
+
+      t.timestamps null: false
+    end
+  end
+end
